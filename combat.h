7c631194fbc1f507c6f8817adc7b92c2451a5e9d
beoulfs <59664930+beoulfs@users.noreply.github.com>
2020-01-13 13:17:15 -0500

    combat.h
    not much here

---
+++ combat.h
@@ -0,0 +1,13 @@
+#ifndef COMBAT_H_INCLUDED
+#define COMBAT_H_INCLUDED
+#include <ctime>
+#include <cstring>
+#include <iostream>
+#include <windows.h>
+#include "chardata.h"
+using namespace std;
+
+void fight(chardata &data1, edata bastrd,spell &s1,spell &s2,spell &s3,spell &s4,spell &s5);
+void spellist (spell &s1,spell &s2,spell &s3,spell &s4,spell &s5);
+
+#endif // COMBAT_H_INCLUDED
